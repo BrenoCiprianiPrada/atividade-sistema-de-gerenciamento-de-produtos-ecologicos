941db8b030c7447838e7deec2a2c211634c24a53
unknown <brenoc.p2007@gmail.com>
2025-03-20 10:54:17 -0300

    adicionei index.ts

---
+++ para-sala/index.ts
@@ -0,0 +1,27 @@
+let numero:number = 10;
+let nome:string = "Breno";
+const pi:number = 3.14;
+
+function saudacao(nome:string, idade:number){
+    console.log(`Olá eu me chamo ${nome} e tenho ${idade} anos`);
+}
+const nomeUsuario:string = "Breno";
+const idade:number = 22;
+saudacao(nomeUsuario,idade);
+
+//OBJETO
+const pessoa = {
+    nome:"Blackouts",
+    idade:24,
+    cidade:"Fornai",
+    apresentar: function():string{
+        return `Olá meu nome é %{this.nome}, tenho ${this.idade} e sou da cidade de ${this.cidade}`
+    }
+}
+
+console.log(pessoa.apresentar());
+
+function somar(a: number, b: number): number{
+    return a + b;
+}
+console.log(somar(3,4));
